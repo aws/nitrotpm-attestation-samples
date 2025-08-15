fb247c54ad4994706207da52ad0040cc8fdbfcb9
Marius Knaust <mknaust@amazon.com>
2025-08-15 16:41:09 +0200

    Add secure boot support
    This commit introduces secure boot capabilities to the image build flow
    and extends the nginx-kms example with an option to enable secure boot.
    
    The implementation integrates EFI binary signing into the build process.
    When secure boot is enabled, the build system automatically signs the
    unified kernel image with the generated keys and computes TPM PCR
    measurements including secure boot variables (PCR7).
    
    The nginx-kms example demonstrates this functionality with a complete
    secure boot setup, including key generation (with a --secure-boot flag).
    Note that the example generates keys at build time, making those builds
    non-reproducible; production use should provide pre-generated key
    material.
    
    Signed-off-by: Marius Knaust <mknaust@amazon.com>

--- nix/examples/nginx-kms/scripts/steps/00_create_ami.sh
+++ nix/examples/nginx-kms/scripts/steps/00_create_ami.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Parse command line arguments
-PACKAGE_NAME="raw-image"
+SECURE_BOOT=false
+DEBUG=false
 while [[ $# -gt 0 ]]; do
   case $1 in
+    --secure-boot)
+      SECURE_BOOT=true
+      shift
+      ;;
     --debug)
-      PACKAGE_NAME="raw-image-debug"
+      DEBUG=true
       shift
       ;;
     *)
@@ -15,7 +20,13 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+# Build the package name based on flags
+PACKAGE_NAME="raw-image"
+[ "$SECURE_BOOT" = true ] && PACKAGE_NAME="${PACKAGE_NAME}-secure-boot"
+[ "$DEBUG" = true ] && PACKAGE_NAME="${PACKAGE_NAME}-debug"
+
 echo "Running Nix UKI build for package: $PACKAGE_NAME"
+
 nix --extra-experimental-features nix-command --extra-experimental-features flakes build .#$PACKAGE_NAME
 
 if [ $? -ne 0 ]; then
@@ -31,7 +42,12 @@ if [ ! -d "result" ]; then
 fi
 
 echo "Creating UKI AMI..."
-nix --extra-experimental-features nix-command --extra-experimental-features flakes run .#create-ami -- result/nixos-tee_1.raw
+
+# Build the AMI creation target name
+CREATE_AMI_TARGET="create-ami"
+[ "$SECURE_BOOT" = true ] && CREATE_AMI_TARGET="${CREATE_AMI_TARGET}-secure-boot"
+
+nix --extra-experimental-features nix-command --extra-experimental-features flakes run .#$CREATE_AMI_TARGET -- result/nixos-tee_1.raw
 
 if [ $? -ne 0 ]; then
   echo "Error: UKI AMI creation failed"
